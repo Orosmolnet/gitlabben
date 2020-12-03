b0b493d87c406bc813acd6af9d04d155a83043db
Orosmolnet <40334519+Orosmolnet@users.noreply.github.com>
2020-12-03 10:46:38 +0100

    Update sok.java

--- sok/src/sok.java
+++ sok/src/sok.java
@@ -5,24 +5,23 @@ import java.io.*;
 import java.lang.*;
 
 
-public class sok
-{
-    public void parseFile(String fileName,String searchStr) throws FileNotFoundException{
+public class sok {
+    public void parseFile(String fileName, String searchStr) throws FileNotFoundException {
         Scanner scan = new Scanner(new File(fileName));
-        int count=1;
-        while(scan.hasNext()){
+        int count = 1;
+
+        while (scan.hasNext()) {
+
             String line = scan.nextLine().toLowerCase().toString();
-            if(line.contains(searchStr)){
 
-                System.out.println("Line "+count);
-
+            if (line.contains(searchStr)) {
+                System.out.println("Line " + count);
             }
             count++;
         }
     }
 
-
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
         sok fileSearch = new sok();
         fileSearch.parseFile("/Users/simon/Documents/GitHub/gitlabben/sok/input.txt", "katt");
     }
