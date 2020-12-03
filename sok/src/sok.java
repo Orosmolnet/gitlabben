b39c9b071a2decaf4b2fa02cf0745665cbb78669
Orosmolnet <40334519+Orosmolnet@users.noreply.github.com>
2020-12-03 10:42:18 +0100

    fixad

--- sok/src/sok.java
+++ sok/src/sok.java
@@ -1,41 +1,30 @@
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
 
 public class sok
 {
-    public static void main(String[] args) throws IOException
-    {
-        File f1=new File("input.txt"); //Creation of File Descriptor for input file
-        String[] words=null;  //Intialize the word Array
-        FileReader fr = new FileReader(f1);  //Creation of File Reader object
-        BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
-        String s;
-        String input="sökord";   // Input word to be searched
-        int count=0;   //Intialize the word to zero
-        while((s=br.readLine())!=null)   //Reading Content from the file
-        {
-            words=s.split(" ");  //Split the word using space
-            for (String word : words)
-            {
-                if (word.equals(input))   //Search for the given word
-                {
-                    count++;
-                }
-            }
-        }
-        if(count!=0)
-        {
-            System.out.println("The given word is present for "+count+ " Times in the file");
-        }
-        else
-        {
-            System.out.println("The given word is not present in the file");
-        }
+    public void parseFile(String fileName,String searchStr) throws FileNotFoundException{
+        Scanner scan = new Scanner(new File(fileName));
+        int count=1;
+        while(scan.hasNext()){
+            String line = scan.nextLine().toLowerCase().toString();
+            if(line.contains(searchStr)){
 
-        fr.close();
+                System.out.println("Line "+count);
+
+            }
+            count++;
+        }
     }
 
+
+    public static void main(String[] args) throws FileNotFoundException{
+        sok fileSearch = new sok();
+        fileSearch.parseFile("/Users/simon/Documents/GitHub/gitlabben/sok/input.txt", "katt");
+    }
 
 }
